f0ba714a775b481522edcbd3b7644fe7fdf9cfca
gushk93 <gustavo.krampe@gmail.com>
2022-08-26 21:00:46 -0300

    Atualizado aula 3

---
+++ tarefa-dia-3/projeto-2.js
@@ -0,0 +1,8 @@
+var tabuada = 7;
+var a = 0;
+
+console.log("TABUADA DO 7")
+while (a<=10) {
+    console.log(`${tabuada} X ${a} = ${tabuada*a}`);
+    a++;
+}
---
+++ tarefa-dia-3/projeto-3.js
@@ -0,0 +1,9 @@
+var a = 1;
+const n = 20;
+
+while (a<=n) {
+    if (a%2 != 0) {
+        console.log(a);
+    }
+    a++
+}
